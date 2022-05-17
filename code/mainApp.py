222fdcb5968031ae4344061a81eac6a6e55cc4b1
MohanadDiab <94736049+MohanadDiab@users.noreply.github.com>
2022-05-17 16:43:49 +0200

    just testing things out

--- code/mainApp.py
+++ code/mainApp.py
@@ -1,11 +1,35 @@
 from psycopg2 import (connect)
-from flask import (Flask,render_template)
+from flask import (
+    Flask,
+    render_template,
+    request,
+    )
 
 app=Flask(__name__,template_folder="templates")
 
 @app.route('/')
 def home():
-    return render_template('Home.html')
+    return render_template('home.html')
+
+@app.route('/viewMap')
+def viewMap():
+    return render_template('viewMap.html')
+
+@app.route('/viewMap2')
+def viewMap2():
+    return render_template('viewMap2.html')
+
+@app.route('/reserveBike',methods=['GET','POST'])
+def reserveBike():
+    if request.method=='POST':
+        return 'Reserved bike number: ' + request.form['bikeNumber']
+    return '''
+<form method="post">
+<p><label>Insert bike number: </label><input type=text name=bikeNumber>
+<p><input type=submit value=Ok>
+</form> '''
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
