d2572bf8ef260d99d93116f7ecd448b5148acfc3
MajedGhazi <majed.bennoman@gmail.com>
2022-06-07 17:35:03 +0200

    ML Algorithm

---
+++ code/ml.py
@@ -0,0 +1,18 @@
+import pickle
+
+import pandas as pd
+from sql import connect_sql
+
+(conn,c) = connect_sql()
+c.execute('select "3_Dwelling_type","4_Number_of_trees","5_Distance_to_major_","6_Decibel_reading","8_Age_of_Property","9_Quality_of_housing" from "Housing Data" where "9_Quality_of_housing" != \'-1\'')
+df = pd.DataFrame(list(c.fetchall()))
+dummies = pd.get_dummies(df, columns = df.columns[:-1])
+print(dummies.columns.values.tolist())
+X_train, y_train=(dummies,df[5])
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+regressor.fit(X_train, y_train)
+pickle.dump(regressor,open("rf.dat","wb"))
